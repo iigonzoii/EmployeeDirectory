d64f9831a7d9343178d3db97d668282c653a0cbe
iigonzoii <a.gonzalez418@gmail.com>
2021-05-09 13:41:41 -0600

    map function is written and printing to screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import {useState, useEffect} from "react"
+import "./App.css";
+import { useEffect, useState } from "react";
 
 // on page load we want to have a table display... make a table in html using jsx to populate in further steps
 
@@ -11,9 +11,8 @@ import {useState, useEffect} from "react"
 // use map to create the table row by row?
 // we need a row created for each person, we need their information displayed in each column
 
-
 // step three figure out what data goes where
-// check out docs on api 
+// check out docs on api
 
 // step 4 put data in form from fetch
 
@@ -25,20 +24,28 @@ import {useState, useEffect} from "react"
 // step 8 read me
 // step 9 refactor after mvp is reached
 function App() {
-  const [employeeArray, setEmployeeArray] = useState([])
+  const [employeeArray, setEmployeeArray] = useState([]);
 
   useEffect(() => {
-fetch('https://randomuser.me/api/?inc=picture,name,phone,email')
-.then((response) => response.json())
-.then((fetchData) => {
-  console.log(fetchData)
-})
+    fetch("https://randomuser.me/api/?results=15")
+      .then((response) => response.json())
+      .then((fetchData) => {
+        console.log(fetchData.results)
+        setEmployeeArray(fetchData.results);
+      });
+      ;
+  }, []);
 
-  }, [])
   return (
-    <div>
-      
-    </div>
+    <>
+      {employeeArray.map((each, index) => {
+        return (
+          <div key={index}>
+            <h2>{each.name.first} {each.name.last} {each.email} {each.cell}</h2>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
