d49bdc2800c8471a6d3745a3c3c5611809ed7fd9
iigonzoii <a.gonzalez418@gmail.com>
2021-05-09 11:37:31 -0600

    some pseudo code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+// on page load we want to have a table display... make a table in html using jsx to populate in further steps
+// step one make a form
+// image, name, phone, email
+// use map to create the table row by row?
+
+
+// step two figure out what data goes where
+// check out docs on api 
+
+
+// step three make a fetch to fill form on page load
+// refer to tutorial components and props for the fetch we built
+
+// step 4 put data in form from fetch
+
+// step 5 filter
+// make search input field that ties to the table and uses filter to update the fields
+
+// step 6 sort
+// step 7 css
+// step 8 read me
+// step 9 refactor after mvp is reached
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
