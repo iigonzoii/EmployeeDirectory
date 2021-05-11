cba792cdef8422a0d6548c59d61ad38a7edcd575
iigonzoii <a.gonzalez418@gmail.com>
2021-05-10 18:13:58 -0600

    finally got filter to work

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import React from "react";
 
 // on page load we want to have a table display... make a table in html using jsx to populate in further steps
+// !CHECK
 
-// step one make a fetch to fill form on page load
+// todostep one make a fetch to fill form on page load
 // refer to tutorial components and props for the fetch we built  **CHECK**
+// !CHECK
 
-// step two make a form
+//todo step two make a form
 // image, name, phone, email
 // use map to create the table row by row?
 // we need a row created for each person, we need their information displayed in each column **CHECK**
+// !CHECK
 
-// step three figure out what data goes where
+//todo step three figure out what data goes where
 // check out docs on api **CHECK**
+// !CHECK
 
-// step 4 put data in form from fetch
+//todo step 4 put data in form from fetch
+// !CHECK
 
-// step 5 filter
+//todo step 5 filter
 // make search input field that ties to the table and uses filter to update the fields
 
-// step 6 sort
-// step 7 css
-// step 8 read me
-// step 9 refactor after mvp is reached
+//todo step 6 sort
+// this will be the carrot that you click and have employees displayed in descending or ascending order
+
+//todo step 7 css
+
+//todo step 8 read me
+
+//todo step 9 refactor after mvp is reached
+
+// ? video walkthrough, css issue,  onchange or handleinput/handle submit? i think another question but i cant remember.
+
 function App() {
   const [employeeArray, setEmployeeArray] = useState([]);
+  const [inputsObj, setInputsObj] = useState({});
 
   useEffect(() => {
-    // ! when putting multiple query params it got really upset and refused to work with me. i had it set to bring back only the things i needed. it kept bringing back objects and wouldnt let me bring back more than one user. spent a bunch of time with react telling me .map isnt a function, when in reality i wasnt getting back an array, i was getting back an object
     fetch("https://randomuser.me/api/?results=15")
       .then((response) => response.json())
       .then((fetchData) => {
@@ -36,33 +49,61 @@ function App() {
       });
   }, []);
 
+  const handleInputs = (e) => {
+    // set our state variable of an empty object to clone
+    // step one we get something back from state
+    var clone = inputsObj;
+    // here we are using [] notation to go through an object because we dont know what the key will be, only the value
+    //step two we do something to state
+    clone[e.target.name] = e.target.value;
+    // here we update our state variable with the new value
+    // step three then we set state back to updated value
+    setInputsObj(clone);
+
+    console.log({ ...inputsObj });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleNameSearch = (e) => {
+    //  here we filter through employee array and we use include not as an array method but as a string method to go through each employees first name and return only employees who match our target value
+    let smallerArray = employeeArray.filter((each) => {
+      return each.name.first.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    console.log(smallerArray);
+  };
+
   return (
     <>
-      
-          <table style={{ width:"100%"}} >
-            <thead>
-            <tr>
-              <th>Picture</th>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Email</th>
-              <th>CellPhone</th>
-            </tr>
-            </thead>
-            </table>
-            {employeeArray.map((each, index) => {
+      {/* name represents the key and "userInput value" represents the input value */}
+      <form onSubmit={handleSubmit}>
+        {/* <input type="text" name="name" onChange={handleInputs} /> */}
+        <input type="text" name="name" onChange={handleNameSearch}/>
+      </form>
+
+      <table style={{ width: "100%" }}>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+            <th>CellPhone</th>
+          </tr>
+        </thead>
+      </table>
+      {employeeArray.map((each, index) => {
         return (
-            <tr key={index}>
-              <td>
-                <img src={each.picture.thumbnail} />
-              </td>
-              <td>{each.name.first}</td>
-              <td>{each.name.last}</td>
-              <td>{each.email}</td>
-              <td>{each.cell}</td>
-            </tr>
-            
-          
+          <tr key={index}>
+            <td>
+              <img src={each.picture.thumbnail} />
+            </td>
+            <td>{each.name.first}</td>
+            <td>{each.name.last}</td>
+            <td>{each.email}</td>
+            <td>{each.cell}</td>
+          </tr>
         );
       })}
     </>
